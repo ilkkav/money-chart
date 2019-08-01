7afc80bc60c14bfe6136bbd518631a16e3eab166
Ilkka Vanhanen <ilkka.vanhanen@reaktor.com>
2019-08-01 14:23:33 +0300

    Rename app.js to App.jsx, fix css bugs

--- client/src/components/FileButton.jsx
+++ client/src/components/FileButton.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function FileButton(props) {
   return (
     <div className="button-container">
-      <label class="file-item" for="file">Select file to import</label>
-      <input class="file-item" name="myFile" type="file" onChange={props.onChange}/>
+      <label className="file-item" htmlFor="file">Select file to import</label>
+      <input className="file-item" name="myFile" type="file" onChange={props.onChange}/>
     </div>
   );
 };
